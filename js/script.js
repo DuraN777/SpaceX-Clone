0d8808fec667714258f9cf698c06963bf1aa2a9e
Djura Neskovic <neskovic991@gmail.com>
2022-09-01 11:15:51 +0200

    mobile menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.mobile-main-menu');
 
 btn.addEventListener('click', navToggle);
+
+window.addEventListener('scroll', scrollFunction);
 
 function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
+  menu.classList.toggle('show-menu');
+}
+
+// Hide Menu on Scroll
+function scrollFunction() {
+  btn.classList.remove('open');
+  overlay.classList.remove('overlay-show');
+  menu.classList.remove('show-menu');
 }

d1708e432eb1a5c55dc828394a5b47f2a641ab94
Djura Neskovic <neskovic991@gmail.com>
2022-09-03 18:51:40 +0200

    animate acting strange, experiment

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-      // Hamburger Menu
+// Hamburger Menu elements
 const btn = document.getElementById('menu-btn');
 const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.mobile-main-menu');
 
 // Inner section elements
-const h4 = document.querySelectorAll('h4');
+const h4 = document.querySelectorAll('.animateH4');
 const h2 = document.querySelectorAll('h2');
-const newBtn = document.querySelectorAll('.btn')
+const newBtn = document.querySelectorAll('.btn');
 
+// toggle hamburger menu and hide on scroll
 btn.addEventListener('click', navToggle);
 window.addEventListener('scroll', scrollFunction);
 
-window.addEventListener('scroll', animateBtn);
-animateBtn();
-window.addEventListener('scroll', animateH4);
-animateH4();
-window.addEventListener('scroll', animateH2);
-animateH2();
+    // function for each tag group
+// window.addEventListener('scroll', animateH4);
+// animateH4();
+// window.addEventListener('scroll', animateH2);
+// animateH2();
+// window.addEventListener('scroll', animateBtn);
+// animateBtn();
 
+// one function to rule them all
+window.addEventListener('scroll', animateTag(h4));
+animateTag(h4);
+window.addEventListener('scroll', animateTag(h2));
+animateTag(h2);
+window.addEventListener('scroll', animateTag(newBtn));
+animateTag(newBtn);
+
+function animateTag(tags) {
+  const triggerBottom = window.innerHeight * 1.1;
+
+  tags.forEach((tag) => {
+  let tagTop = tag.getBoundingClientRect().top;
+    if(tagTop < triggerBottom) {
+      tag.classList.add('show');
+    }
+  })
+}
+
+      /* Functions */
+
+//Toggle hamburger menu
 function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
@@ -67,3 +91,15 @@ function animateBtn() {
     }
   })
 }
+
+
+// function animateTag(tags) {
+//   const triggerBottom = window.innerHeight * 1.1;
+
+//   tags.forEach((tag) => {
+//   let tagTop = tag.getBoundingClientRect().top;
+//     if(tagTop < triggerBottom) {
+//       tag.classList.add('show');
+//     }
+//   })
+// }
